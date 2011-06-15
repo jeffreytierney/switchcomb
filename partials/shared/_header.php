ea5153a286cc2562e2c3498a67913c48bfd976ad
Jeff Tierney <jeff@tierney.me>
2011-06-15 00:07:57 -0400

    update some css and fix the header a bit

--- partials/shared/_header.php
+++ partials/shared/_header.php
@@ -1,12 +1,12 @@
 
-<div id="header" class="clearfix">
-	<a href="<?php echo SC::root() ?>" id="header_home"><img src="<?php echo SC::imagePath("logo.png"); ?>" alt="" border="0"></a>
+<div id="header">
+	<a href="<?php echo SC::root() ?>" id="header_home">Switchcomb</a>
   <?php if($current_user): ?>
     <div id="loggedin" class="clearfix">
-      <span id="loggedinuser"><?php echo $current_user->displayname ?></span>
-      <a href="<?php echo SCRoutes::set("usersessions", "delete"); ?>">Logout</a>
-    </div>
-    <div id="header_links">
+        <div id="auth_container">
+          <span id="loggedinuser"><?php echo $current_user->displayname ?></span>
+          <a href="<?php echo SCRoutes::set("usersessions", "delete"); ?>">Logout</a>
+        </div>
       <ul id="links" class="clearfix">
         <li><a href="<?php echo SCRoutes::set("users", "memberships_index", array("userid"=>$current_user->userid)); ?>" class="header_link logged_in" id="header_boards">My Boards</a></li>
         <li><a href="<?php echo SCRoutes::set("boards", "_new"); ?>" class="header_link logged_in" id="header_create">Create a Board</a></li>
